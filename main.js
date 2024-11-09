4648104559004d5608edbfd9418c9383d3f4df26
Makster20 <mikhaelsaidshah123@gmail.com>
2024-11-09 18:55:07 +0400

    Convert all colors in colors array to rgb() from hexadecimal

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const container = document.querySelector('.container');
 const colors = [
-    'rgb(255, 255, 255)',  // #FFFFFF
-    'rgb(223, 242, 235)',  // #DFF2EB
-    'rgb(185, 229, 232)',  // #B9E5E8
-    'rgb(122, 178, 211)',  // #7AB2D3
-    'rgb(74, 98, 138)',    // #4A628A
-    'rgb(122, 178, 211)',  // #7AB2D3
-    'rgb(185, 229, 232)',  // #B9E5E8
-    'rgb(223, 242, 235)'   // #DFF2EB
+    ['rgb(255, 255, 255)', false],  // #FFFFFF
+    ['rgb(223, 242, 235)', false],  // #DFF2EB
+    ['rgb(185, 229, 232)', false],  // #B9E5E8
+    ['rgb(122, 178, 211)', false], // #7AB2D3
+    ['rgb(74, 98, 138)', false],  // #4A628A
+    ['rgb(122, 178, 211)', false], // #7AB2D3
+    ['rgb(185, 229, 232)', false], // #B9E5E8
+    ['rgb(223, 242, 235)', true] // #DFF2EB
 ];
 
 for(let i = 0; i < 16; i++){
@@ -22,12 +22,11 @@ for(let i = 0; i < 16; i++){
 let boxes = document.querySelectorAll('.box');
 boxes.forEach(function(box){
     box.addEventListener('mouseover', function(){
-        if(box.style.backgroundColor === colors[colors.length - 1]){
-            box.style.backgroundColor = colors[0];
-            console.log('1212121212')
+        if(box.style.backgroundColor === colors[colors.length - 1][0]){
+            box.style.backgroundColor = colors[0][0];
+            console.log('yes')
         }else{
-        box.style.backgroundColor = colors[colors.indexOf(box.style.backgroundColor) + 1];
-        console.log(box.style.backgroundColor)
+            
         }
     })
 });
